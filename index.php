c82e605bf6ff4d7b87bee74314a11e56f501e515
John Plastow <j2trumpet@gmail.com>
2020-11-20 15:27:07 -0800

    Conversion to @mixin instead of @extend
    Dropped all PHP code

--- index.php
+++ index.php
@@ -12,20 +12,38 @@
     }
     fieldset {
       margin: 25px 0;
+      border-style: solid;
+      border-color: #383F43;
     }
-    .cru-button {
+    #button-fieldset {
+      padding-left: 0;
+      padding-right: 0;
+      padding-bottom: 0;
+    }
+    #button-fieldset legend {
+      margin-left: 12px;
+    }
+    .cru-button, .cru-tag, .cru-dot {
+      display: inline-block;
+      margin-left: 12px;
       margin-bottom: 5px;
-    }
-    .dark-bg {
-      padding: 10px;
-      background-color: #383F43;
     }
     .div-sep {
       margin-top: 10px;
       margin-bottom: 10px;
     }
-    .div-sep .cru-button:nth-child(4),
-    .div-sep .cru-button:nth-child(5) { margin-bottom: 0; }
+    .div-sep:last-child {
+      margin-bottom: 0;
+    }
+    .dark-bg {
+      padding-top: 12px;
+      padding-right: 12px;
+      padding-bottom: 7px;
+      background-color: #383F43;
+    }
+    .dark-bg:last-child {
+      padding-bottom: 5px;
+    }
   </style>
 </head>
 
@@ -40,95 +58,134 @@
 
     <fieldset>
       <legend>Title</legend>
-      <?php
-        $heading_styles = ['xxl', 'xl', 'lg', 'md', 'sm', 'xs'];
-        foreach ($heading_styles as $heading) {
-          echo '<div class="cru-heading cru-heading-' . $heading . '">Heading ' . strtoupper($heading) . '</div>' . "\n";
-        }
-      ?>
+      <div class="cru-heading cru-heading-xxl">Heading XX Large</div>
+      <div class="cru-heading cru-heading-xl">Heading Extra Large</div>
+      <div class="cru-heading cru-heading-lg">Heading Large</div>
+      <div class="cru-heading cru-heading-md">Heading Medium</div>
+      <div class="cru-heading cru-heading-sm">Heading Small</div>
+      <div class="cru-heading cru-heading-xs">Heading Extra Small</div>
     </fieldset>
 
-    <fieldset>
+    <fieldset id="button-fieldset">
       <legend>Button</legend>
-      <?php
-        $button_variations = [
-          ['', 'Default'],
-          ['yellow-gray', 'Yellow Gray'],
-          ['gray-white', 'Gray White'],
-          ['white-gray', 'White Gray', true],
-          ['yellow-gray-outline', 'Yellow Gray Outline'],
-          ['gray-gray-outline', 'Gray Gray Outline'],
-          ['yellow-white-outline', 'Yellow White Outline', true],
-          ['white-white-outline', 'White White Outline', true],
-        ];
-
-        $tag_variations = [
-          $button_variations[2],
-          $button_variations[3],
-          $button_variations[5],
-          $button_variations[7],
-        ];
-
-        function display_buttons($buttons, $type = 'button') {
-          foreach ($buttons as $button) {
-            $background = ($button[2]) ? ' dark-bg' : '';
-            echo '<div class="div-sep' . $background . '">' . "\n";
-            for ($i = 1; $i <= 5; $i++) {
-              if ($i === 3) {
-                echo '<br />' . "\n";
-                continue;
-              }
-
-              $label = $button[1];
-              $classes = ($button[0]) ? ' cru-button-' . $button[0] : '';
-              $disabled = ($i === 2 || $i === 5) ? ' disabled' : '';
-              $icon = ($i === 4 || $i === 5) ? ' Icon <i class="fal fa-shopping-cart"></i>' : '';
-
-              if ($type == 'tag') {
-                $label = 'Recommended';
-                $classes .= ' cru-tag';
-                $icon = ($icon) ? ' <i class="fas fa-times-circle"></i>' : '';
-                if ($icon) {
-                  $classes .= ' cru-tag-icon';
-                }
-              }
-              
-              echo '<button class="cru-button' . $classes . '"' . $disabled . '>' . $label . $icon . '</button>' . "\n";
-            }
-            echo '</div>' . "\n";
-          }
-        }
-        display_buttons($button_variations);
-        display_buttons($tag_variations, 'tag');
-
-        $dot_variations = [
-          $button_variations[3],
-          $button_variations[5],
-          $button_variations[7],
-        ];
-
-        function display_dots($dots) {
-          $arrows = ['up', 'left', 'right', 'down'];
-          foreach ($dots as $dot) {
-            $background = ($dot[2] && $dot[0] != 'white-gray') ? ' dark-bg' : '';
-            echo '<div class="div-sep' . $background . '">' . "\n";
-            foreach ($arrows as $arrow) {
-              echo '<button class="cru-button cru-button-' . $dot[0] . ' cru-dot"><i class="fal fa-arrow-' . $arrow . '"></i></button>' . "\n";
-            }
-            echo '<br />' . "\n";
-            foreach ($arrows as $arrow) {
-              echo '<button class="cru-button cru-button-' . $dot[0] . ' cru-dot disabled"><i class="fal fa-arrow-' . $arrow . '"></i></button>' . "\n";
-            }
-            echo '</div>' . "\n";
-          }
-        }
-        display_dots($dot_variations);
-      ?>
+      <!-- Standard buttons -->
+      <div class="div-sep">
+        <button class="cru-button cru-button-yellow-gray">Yellow Gray</button>
+        <button class="cru-button cru-button-yellow-gray" disabled>Yellow Gray</button>
+        <br>
+        <button class="cru-button cru-button-yellow-gray">Yellow Gray Icon <i class="fal fa-shopping-cart"></i></button>
+        <button class="cru-button cru-button-yellow-gray" disabled>Yellow Gray Icon <i class="fal fa-shopping-cart"></i></button>
+      </div>
+      <div class="div-sep">
+        <button class="cru-button cru-button-gray-white">Gray White</button>
+        <button class="cru-button cru-button-gray-white" disabled>Gray White</button>
+        <br>
+        <button class="cru-button cru-button-gray-white">Gray White Icon <i class="fal fa-shopping-cart"></i></button>
+        <button class="cru-button cru-button-gray-white" disabled>Gray White Icon <i class="fal fa-shopping-cart"></i></button>
+      </div>
+      <div class="div-sep dark-bg">
+        <button class="cru-button cru-button-white-gray">White Gray</button>
+        <button class="cru-button cru-button-white-gray" disabled>White Gray</button>
+        <br>
+        <button class="cru-button cru-button-white-gray">White Gray Icon <i class="fal fa-shopping-cart"></i></button>
+        <button class="cru-button cru-button-white-gray" disabled>White Gray Icon <i class="fal fa-shopping-cart"></i></button>
+      </div>
+      <div class="div-sep">
+        <button class="cru-button cru-button-yellow-gray-outline">Yellow Gray Outline</button>
+        <button class="cru-button cru-button-yellow-gray-outline" disabled>Yellow Gray Outline</button>
+        <br>
+        <button class="cru-button cru-button-yellow-gray-outline">Yellow Gray Outline Icon <i class="fal fa-shopping-cart"></i></button>
+        <button class="cru-button cru-button-yellow-gray-outline" disabled>Yellow Gray Outline Icon <i class="fal fa-shopping-cart"></i></button>
+      </div>
+      <div class="div-sep">
+        <button class="cru-button cru-button-gray-gray-outline">Gray Gray Outline</button>
+        <button class="cru-button cru-button-gray-gray-outline" disabled>Gray Gray Outline</button>
+        <br>
+        <button class="cru-button cru-button-gray-gray-outline">Gray Gray Outline Icon <i class="fal fa-shopping-cart"></i></button>
+        <button class="cru-button cru-button-gray-gray-outline" disabled>Gray Gray Outline Icon <i class="fal fa-shopping-cart"></i></button>
+      </div>
+      <div class="div-sep dark-bg">
+        <button class="cru-button cru-button-yellow-white-outline">Yellow White Outline</button>
+        <button class="cru-button cru-button-yellow-white-outline" disabled>Yellow White Outline</button>
+        <br>
+        <button class="cru-button cru-button-yellow-white-outline">Yellow White Outline Icon <i class="fal fa-shopping-cart"></i></button>
+        <button class="cru-button cru-button-yellow-white-outline" disabled>Yellow White Outline Icon <i class="fal fa-shopping-cart"></i></button>
+      </div>
+      <div class="div-sep dark-bg">
+        <button class="cru-button cru-button-white-white-outline">White White Outline</button>
+        <button class="cru-button cru-button-white-white-outline" disabled>White White Outline</button>
+        <br>
+        <button class="cru-button cru-button-white-white-outline">White White Outline Icon <i class="fal fa-shopping-cart"></i></button>
+        <button class="cru-button cru-button-white-white-outline" disabled>White White Outline Icon <i class="fal fa-shopping-cart"></i></button>
+      </div>
+      <!-- Tag-style buttons -->
+      <div class="div-sep">
+        <button class="cru-tag cru-tag-gray-white">Recommended</button>
+        <button class="cru-tag cru-tag-gray-white" disabled>Recommended</button>
+        <br>
+        <button class="cru-tag cru-tag-gray-white cru-tag-icon">Recommended <i class="fas fa-times-circle"></i></button>
+        <button class="cru-tag cru-tag-gray-white cru-tag-icon" disabled>Recommended <i class="fas fa-times-circle"></i></button>
+      </div>
+      <div class="div-sep dark-bg">
+        <button class="cru-tag cru-tag-white-gray">Recommended</button>
+        <button class="cru-tag cru-tag-white-gray" disabled>Recommended</button>
+        <br>
+        <button class="cru-tag cru-tag-white-gray cru-tag-icon">Recommended <i class="fas fa-times-circle"></i></button>
+        <button class="cru-tag cru-tag-white-gray cru-tag-icon" disabled>Recommended <i class="fas fa-times-circle"></i></button>
+      </div>
+      <div class="div-sep">
+        <button class="cru-tag cru-tag-gray-gray-outline">Recommended</button>
+        <button class="cru-tag cru-tag-gray-gray-outline" disabled>Recommended</button>
+        <br>
+        <button class="cru-tag cru-tag-gray-gray-outline cru-tag-icon">Recommended <i class="fas fa-times-circle"></i></button>
+        <button class="cru-tag cru-tag-gray-gray-outline cru-tag-icon" disabled>Recommended <i class="fas fa-times-circle"></i></button>
+      </div>
+      <div class="div-sep dark-bg">
+        <button class="cru-tag cru-tag-white-white-outline">Recommended</button>
+        <button class="cru-tag cru-tag-white-white-outline" disabled>Recommended</button>
+        <br>
+        <button class="cru-tag cru-tag-white-white-outline cru-tag-icon">Recommended <i class="fas fa-times-circle"></i></button>
+        <button class="cru-tag cru-tag-white-white-outline cru-tag-icon" disabled>Recommended <i class="fas fa-times-circle"></i></button>
+      </div>
+      <!-- Dot-style buttons -->
+      <div class="div-sep">
+        <button class="cru-dot cru-dot-white-gray"><i class="fal fa-arrow-up"></i></button>
+        <button class="cru-dot cru-dot-white-gray"><i class="fal fa-arrow-left"></i></button>
+        <button class="cru-dot cru-dot-white-gray"><i class="fal fa-arrow-right"></i></button>
+        <button class="cru-dot cru-dot-white-gray"><i class="fal fa-arrow-down"></i></button>
+        <br>
+        <button class="cru-dot cru-dot-white-gray disabled"><i class="fal fa-arrow-up"></i></button>
+        <button class="cru-dot cru-dot-white-gray disabled"><i class="fal fa-arrow-left"></i></button>
+        <button class="cru-dot cru-dot-white-gray disabled"><i class="fal fa-arrow-right"></i></button>
+        <button class="cru-dot cru-dot-white-gray disabled"><i class="fal fa-arrow-down"></i></button>
+      </div>
+      <div class="div-sep">
+        <button class="cru-dot cru-dot-gray-gray-outline"><i class="fal fa-arrow-up"></i></button>
+        <button class="cru-dot cru-dot-gray-gray-outline"><i class="fal fa-arrow-left"></i></button>
+        <button class="cru-dot cru-dot-gray-gray-outline"><i class="fal fa-arrow-right"></i></button>
+        <button class="cru-dot cru-dot-gray-gray-outline"><i class="fal fa-arrow-down"></i></button>
+        <br>
+        <button class="cru-dot cru-dot-gray-gray-outline disabled"><i class="fal fa-arrow-up"></i></button>
+        <button class="cru-dot cru-dot-gray-gray-outline disabled"><i class="fal fa-arrow-left"></i></button>
+        <button class="cru-dot cru-dot-gray-gray-outline disabled"><i class="fal fa-arrow-right"></i></button>
+        <button class="cru-dot cru-dot-gray-gray-outline disabled"><i class="fal fa-arrow-down"></i></button>
+      </div>
+      <div class="div-sep dark-bg">
+        <button class="cru-dot cru-dot-white-white-outline"><i class="fal fa-arrow-up"></i></button>
+        <button class="cru-dot cru-dot-white-white-outline"><i class="fal fa-arrow-left"></i></button>
+        <button class="cru-dot cru-dot-white-white-outline"><i class="fal fa-arrow-right"></i></button>
+        <button class="cru-dot cru-dot-white-white-outline"><i class="fal fa-arrow-down"></i></button>
+        <br>
+        <button class="cru-dot cru-dot-white-white-outline disabled"><i class="fal fa-arrow-up"></i></button>
+        <button class="cru-dot cru-dot-white-white-outline disabled"><i class="fal fa-arrow-left"></i></button>
+        <button class="cru-dot cru-dot-white-white-outline disabled"><i class="fal fa-arrow-right"></i></button>
+        <button class="cru-dot cru-dot-white-white-outline disabled"><i class="fal fa-arrow-down"></i></button>
+      </div>
     </fieldset>
 
-    <button class="test">Hi! I'm a button!</button>
-    <button class="test-tag">Hi! I'm a tag! <i class="fas fa-times-circle"></i></button>
-    <button class="dot"><i class="fal fa-arrow-right"></i></button>
+    <button class="button-test">Hi! I'm a button!</button>
+    <button class="tag-test">Hi! I'm a tag! <i class="fas fa-times-circle"></i></button>
+    <button class="dot-test"><i class="fal fa-arrow-right"></i></button>
   
   </div>
 </body>
