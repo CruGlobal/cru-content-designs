1fe25bdd5ba71b2fd9bf5859d0db5a6f31e5a742
Sebastian Melzi <sebastian.melzi@qxbits.com>
2021-03-12 15:30:18 -0300

    Added php files

---
+++ v0.3/cruorg/components/separator/separator.php
@@ -0,0 +1,14 @@
+<div style="border: 1px solid black; margin: 30px 0">
+    <div class="separator cru-separator-ligth">
+      <div id="separator-cc11e67e10" class="cmp-separator">
+        <hr class="cmp-separator__horizontal-rule">
+      </div>
+    </div>
+</div>
+<div style="border: 1px solid white; margin: 30px 0; background: #383F43">
+    <div class="separator">
+      <div id="separator-cc11e67e10" class="cmp-separator cru-separator-white">
+        <hr class="cmp-separator__horizontal-rule">
+      </div>
+    </div>
+</div>
