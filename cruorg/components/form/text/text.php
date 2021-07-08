ae5e32683915eaf19d4902c52b1ec916498c9888
John Plastow <j2trumpet@gmail.com>
2021-07-07 18:51:20 -0700

    Fixing right padding for type-specific form fields

--- cruorg/components/form/text/text.php
+++ cruorg/components/form/text/text.php
@@ -2,23 +2,33 @@
 include ROOT . '/components/form/form.php';
 
 function cru_form_input($input_type) {
+  $types = ['text', 'textarea', 'email', 'tel', 'date', 'number', 'password'];
+  foreach ($types as $type) :
 ?>
   <div class="text<?php if ($input_type != '') {echo " cru-form-text--$input_type";} ?>">
     <div class="cmp-form-text">
-      <label>Label</label>
-      <input class="cmp-form-text__text" type="text" placeholder="Text goes here..." name="firstName">
-      <?php if ($input_type == 'alert') {
-        // echo '<span>Error message...</span>';
-      } ?>
+      <label><?= ucfirst($type); ?> Label</label>
+      <p class="cmp-form-text__help-block">Help text.</p>
+      <?php if ( $type != 'textarea' ) : ?>
+        <input class="cmp-form-text__text" type="<?= $type ?>" placeholder="Text goes here..." name="<?= $type ?>">
+      <?php else : ?>
+        <textarea class="cmp-form-text__textarea" name="xyz" rows="2"></textarea>
+      <?php endif; ?>
     </div>
   </div>
 <?php
+  endforeach;
 }
 ?>
 
 <div class="info">
-  <p>The classes that affect the form field components actually go in two different places. <code>cru-form-standard</code>, <code>cru-form-underline</code>, and <code>cru-form-dark</code> need to be applied to the form container component. The author should select only one of either <code>cru-form-standard</code> or <code>cru-form-underline</code>. This ensures that every text field in the form uses the same style. <code>cru-form-dark</code> is optional and intended for use on dark backgrounds.</p>
-  <p><code>cru-form-text-disabled</code> and <code>cru-form-text-alert</code> are meant to be applied to the text field components directly, though those classes are really just placeholders until a functional form is enabled and the classes/attributes that get applied by AEM can be observed.</p>
+  <p><strong>Form Container</strong><br />
+  Default CSS Classes: <code>cru-form</code><br />
+  Underlined: <code>cru-form cru-form--underlined</code> (optional)<br />
+  Dark: <code>cru-form cru-form--dark</code> (optional)</p>
+  <p><strong>Form Text</strong><br />
+  Disabled: <code>cru-form-text--disabled</code> (optional)<br />
+  Alert: <code>cru-form-text--alert</code> (optional)</p>
 </div>
 
 <div class="comp-wrapper">
