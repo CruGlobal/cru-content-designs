6c34b0f97242efed7a4d73594f0170a6fa564bd8
John Plastow <j2trumpet@gmail.com>
2022-06-02 12:30:20 -0700

    Deleting old commented-out code

--- give/src/components/shared.js
+++ give/src/components/shared.js
@@ -21,12 +21,6 @@ export const ComponentWrapper = ({
       <DynamicInnerTag className={`cmp-${type}`}>{children}</DynamicInnerTag>
     );
   }
-
-  // return (
-  //   <div className={classes.trim()}>
-  //     <DynamicInnerTag className={`cmp-${type}`}>{children}</DynamicInnerTag>
-  //   </div>
-  // );
 };
 
 export const LinkWrapper = ({ href, className, children }) => {
