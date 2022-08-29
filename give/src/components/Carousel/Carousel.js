766b54da8f6ac0e2fadb5c393ccaf183e1989020
John Plastow <j2trumpet@gmail.com>
2022-08-29 14:55:24 -0700

    Carousel overhaul 2

--- give/src/components/Carousel/Carousel.js
+++ give/src/components/Carousel/Carousel.js
@@ -5,97 +5,110 @@
 import { ComponentWrapper } from "../shared";
 import { Image } from "../Image/Image";
 import "./Carousel.css";
-import "../Button/Button.css";
 import lava1 from "../../assets/lava1.jpeg";
 import lava2 from "../../assets/lava2.jpeg";
 
 export const Carousel = ({
-  className = "", // string
-  navButtonClasses = "", // string - classes for button styling
-  content = [], // array of objects with `slide`, `label`, and `active` keys (slide: Component, label: string, active: bool)
+  className, // string
+  id, // string
+  children,
 }) => {
-  if (content.length === 0) return null;
-
-  const Actions = () => {
-    const actions = [
-      { label: "Previous", classes: "previous" },
-      { label: "Next", classes: "next" },
-      { label: "Pause", classes: "pause" },
-      { label: "Play", classes: "play cmp-carousel__action--disabled" },
-    ];
-
-    return (
-      <div className="cmp-carousel__actions">
-        {actions.map((action, index) => {
-          return (
-            <button
-              className={`cmp-carousel__action cmp-carousel__action--${action.classes}`}
-              key={index}
-            >
-              <span className="cmp-carousel__action-icon"></span>
-              <span className="cmp-carousel__action-text">{action.label}</span>
-            </button>
-          );
-        })}
-      </div>
-    );
-  };
-
-  const Indicators = () => {
-    return (
-      <ol className="cmp-carousel__indicators">
-        {content.map((slide, index) => {
-          const active = slide.active ? " cmp-carousel__indicator--active" : "";
-
-          return (
-            <li className={`cmp-carousel__indicator${active}`} key={index}>
-              {slide.label}
-            </li>
-          );
-        })}
-      </ol>
-    );
-  };
+  if (!children) return null;
 
   return (
-    <ComponentWrapper
-      type="carousel"
-      className={`${navButtonClasses} ${className}`}
-    >
-      <div className="cmp-carousel__content">
-        {content.map((slide, index) => {
-          const active = slide.active ? " cmp-carousel__item--active" : "";
-
-          return (
-            <div className={`cmp-carousel__item${active}`} key={index}>
-              {slide.content}
-            </div>
-          );
-        })}
-        <Actions />
-        <Indicators />
-      </div>
+    <ComponentWrapper type="carousel" className={className} id={id}>
+      <div className="cmp-carousel__content">{children}</div>
     </ComponentWrapper>
   );
 };
 
-export const CarouselExamples = () => {
+export const CarouselItem = ({ id, active = false, children }) => {
+  if (!children) return null;
+
   return (
-    <>
-      <Carousel
-        navButtonClasses="cru-button-dot cru-button-solid cru-button-yellow-gray"
-        content={[
-          {
-            content: <Image src={lava1} alt="Lava 1" />,
-            label: "Slide 1",
-            active: true,
-          },
-          {
-            content: <Image src={lava2} alt="Lava 2" />,
-            label: "Slide 2",
-          },
-        ]}
-      />
-    </>
+    <div
+      className={`cmp-carousel__item${
+        active ? " cmp-carousel__item--active" : ""
+      }`}
+      id={`${id}-tabpanel`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const CarouselActions = () => {
+  const Action = ({ label, classes }) => (
+    <button
+      className={`cmp-carousel__action cmp-carousel__action--${classes}`}
+      type="button"
+      key={label}
+    >
+      <span className="cmp-carousel__action-icon"></span>
+      <span className="cmp-carousel__action-text">{label}</span>
+    </button>
+  );
+
+  return (
+    <div className="cmp-carousel__actions">
+      <Action label="Previous" classes="previous" />
+      <Action label="Next" classes="next" />
+      <Action label="Pause" classes="pause" />
+      <Action label="Play" classes="play cmp-carousel__action--disabled" />
+    </div>
+  );
+};
+
+export const CarouselIndicators = ({ children }) => {
+  if (!children) return null;
+
+  return <ol className="cmp-carousel__indicators">{children}</ol>;
+};
+
+export const CarouselIndicator = ({ id, active = false, children }) => {
+  if (!children) return null;
+
+  return (
+    <li
+      className={`cmp-carousel__indicator${
+        active ? " cmp-carousel__indicator--active" : ""
+      }`}
+      id={`${id}-tab`}
+    >
+      {children}
+    </li>
+  );
+};
+
+export const CarouselExamples = () => {
+  const content = [
+    {
+      content: <Image src={lava1} alt="Lava 1" />,
+      id: "slide1",
+    },
+    {
+      content: <Image src={lava2} alt="Lava 2" />,
+      id: "slide2",
+    },
+  ];
+
+  return (
+    <Carousel id="testID" className="testClass">
+      {content.map((item, index) => (
+        <CarouselItem id={item.id} active={index === 0} key={index}>
+          {item.content}
+        </CarouselItem>
+      ))}
+      <CarouselActions />
+      <CarouselIndicators>
+        {content.map((item, index) => (
+          <CarouselIndicator
+            id={item.id}
+            active={index === 0}
+            key={index}
+          >{`Slide ${index + 1}`}</CarouselIndicator>
+        ))}
+      </CarouselIndicators>
+    </Carousel>
   );
 };
